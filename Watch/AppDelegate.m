e716df8b71432c01dac04c74004a27cd369e3177
andreasandersson <andreas.andersson@appinmed.com>
2015-02-22 23:13:38 +0100

    Code commit

--- Watch/AppDelegate.m
+++ Watch/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"%@ - %@",NSStringFromClass(self.class),NSStringFromSelector(_cmd));
     return YES;
 }
 
