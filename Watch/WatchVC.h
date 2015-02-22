dc505d4dfdf6b76e0f1f72c6dd94080920b7247b
andreasandersson <andreas.andersson@appinmed.com>
2015-02-22 23:40:51 +0100

    Add VC

---
+++ Watch/WatchVC.h
@@ -0,0 +1,13 @@
+//
+//  WatchVC.h
+//  Watch
+//
+//  Created by Andreas Andersson on 22/02/15.
+//  Copyright (c) 2015 AppInMed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WatchVC : UIViewController
+
+@end
