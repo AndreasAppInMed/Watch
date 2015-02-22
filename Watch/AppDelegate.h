a417672e69dfc26a8b806d72f3cf5407f645786a
andreasandersson <andreas.andersson@appinmed.com>
2015-02-22 18:42:38 +0100

    Initial Commit

---
+++ Watch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Watch
+//
+//  Created by Andreas Andersson on 22/02/15.
+//  Copyright (c) 2015 AppInMed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
